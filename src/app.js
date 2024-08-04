1c708b9f555170febb9d08c5a89e01f9ce6164bb
codax69 <pritbusiness03@gmail.com>
2024-08-04 19:11:42 +0530

    beta 1.0.2

--- src/app.js
+++ src/app.js
@@ -7,25 +7,13 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(cookieParser({}));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://ictsm-attendance.vercel.app"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors({
+  origin: 'https://ictsm-attendance.vercel.app/', 
+  methods: ['GET', 'POST','PATCH', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'] 
+}));
 
-app.use(
-  cors({
-    origin:
-      `${process.env.CORS_ORIGIN}` || "https://ictsm-attendance.vercel.app/",
-    credentials: true,
-  })
-);
+
 app.use(express.static("public"));
 
 import userRouter from "./routes/user.routes.js";
