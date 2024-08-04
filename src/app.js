22319bf20bda6ca81afcc663bd56d32fdaa46257
codax69 <pritbusiness03@gmail.com>
2024-08-04 21:07:38 +0530

    beta 1.0.0

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(cookieParser({}));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cors({
-  origin: 'https://ictsm-attendance.vercel.app/', 
+  origin: '*', 
   methods: ['GET', 'POST','PATCH', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'] 
 }));
