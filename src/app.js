6eb6fa3669daf6d9561a9dff027c773d6a64a140
codax69 <pritbusiness03@gmail.com>
2024-08-04 17:39:14 +0530

    beta test 1.0.0

--- src/app.js
+++ src/app.js
@@ -1,19 +1,36 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from 'cors'
+import cors from "cors";
 
 const app = express();
 
-app.use(express.json({limit:"16kb"}));
-app.use(cookieParser({}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(cors({origin:`${process.env.CORS_ORIGIN}`||"*",credentials:true}))
-app.use(express.static("public"))
+app.use(express.json({ limit: "16kb" }));
+app.use(cookieParser({}));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://ictsm-attendance.vercel.app"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
+app.use(
+  cors({
+    origin:
+      `${process.env.CORS_ORIGIN}` || "https://ictsm-attendance.vercel.app",
+    credentials: true,
+  })
+);
+app.use(express.static("public"));
 
-import userRouter from "./routes/user.routes.js"
-import attendanceRouter from "./routes/attendance.routes.js"
-app.use("/api/v1/user",userRouter)
-app.use("/api/v1/",attendanceRouter)
+import userRouter from "./routes/user.routes.js";
+import attendanceRouter from "./routes/attendance.routes.js";
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/", attendanceRouter);
 
-export{app}
+export { app };
