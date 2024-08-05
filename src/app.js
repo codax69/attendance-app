d8b5684485d4c0899e19bc5b3a08f9018e7662a4
Pritesh Patel <128021116+codax69@users.noreply.github.com>
2024-08-05 19:52:17 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(cookieParser({}));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cors({
-  origin: 'https://ictsmattendance.netlify.app/', 
+  origin: 'https://ictsmattendance.netlify.app/ || "*', 
   methods: ['GET', 'POST','PATCH', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'] 
 }));
