68c212c39bb170f6098d14c227a2b79cc98519fd
Pritesh Patel <pritbusiness03@gmail.com>
2024-09-11 13:01:06 +0530

    server update

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(cookieParser({}));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cors({
-  origin: "*", 
+  origin: "https://attendance-app-1.onrender.com", 
   methods: ['GET', 'POST','PATCH', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'] 
 }));
